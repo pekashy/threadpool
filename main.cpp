fa0102b29ed9330b4bd212eac0ad5d4148be8cc0
Egor Zyazev <zyazev.ea@phystech.edu>
2019-12-15 23:50:50 +0300

    First implementation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,127 @@
-#include "threadpool.h"
+#include "threadpool.hpp"
 
 #include <iostream>
 #include <unistd.h>
-#include <zconf.h>
+#include <fstream>
+
+
+using std::cin;
+using std::cout;
+
+template<typename T>
+struct arg {
+    arg() = default;
+
+    arg(T val) {
+        *array = val;
+        len = 1;
+    }
+
+    arg(T *vals) {
+        len = sizeof(vals);
+        array = vals;
+    }
+
+    arg(T *arr, T *buff, int l, int m, int r) : array(arr), buffer(buff), left(l), middle(m), right(r) {}
+
+    T operator()(T val) {
+        *array = val;
+        len = 1;
+    }
+
+    T *array;
+    T *buffer;
+    int left;
+    int middle;
+    int right;
+    size_t len;
+};
+
+int n;
+
+
+template<typename T>
+void merge(void *args) {
+
+    T *array = ((arg<T> *) args)->array;
+    T *buffer = ((arg<T> *) args)->buffer;
+    int left = ((arg<T> *) args)->left;
+    int middle = ((arg<T> *) args)->middle;
+    int right = ((arg<T> *) args)->right;
+
+    int pos_left = 0;
+    int pos_right = 0;
+
+    while (left + pos_left < middle && middle + pos_right < right) {
+        if (array[left + pos_left] < array[middle + pos_right]) {
+            buffer[pos_left + pos_right] = std::move(array[left + pos_left]);
+            pos_left++;
+        } else {
+            buffer[pos_left + pos_right] = std::move(array[middle + pos_right]);
+            pos_right++;
+        }
+    }
+
+    while (left + pos_left < middle) {
+        buffer[pos_left + pos_right] = std::move(array[left + pos_left]);
+        pos_left++;
+    }
+
+    while (middle + pos_right < right) {
+        buffer[pos_left + pos_right] = std::move(array[middle + pos_right]);
+        pos_right++;
+    }
+
+    for (int i = 0; i < pos_left + pos_right; i++)
+        array[left + i] = std::move(buffer[i]);
+}
+
+template<typename T>
+void merge_sort(ThreadPool &tp, std::vector<arg<T> *> &vec_pointer, T *array, T *buffer, int left, int right) {
+    if (right - left <= 1) {
+        return;
+    }
+
+    int middle = left + (right - left) / 2;
+
+    merge_sort(tp, vec_pointer, array, buffer, left, middle);
+    merge_sort(tp, vec_pointer, array, buffer, middle, right);
+    auto *args = new arg<T>(array, buffer, left, middle, right);
+    tp.submit(merge<T>, args);
+    vec_pointer.push_back(args);
+}
+
 
 int main() {
-    int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    std::cin >> n;
+
+    int *arr = new int[n];
+    int *buff = new int[n];
+
+    std::ifstream ifile("input");
+    ifile >> n;
+
+    for (int i = 0; i < n; i++) {
+        ifile >> arr[i];
+    }
+
+    ifile.close();
+
+
+    ThreadPool tp;
+    std::vector<arg<int> *> vec_pointer; // keeping track of memory we allocated
+    merge_sort(tp, vec_pointer, arr, buff, 0, n);
+    tp.finish_work(); // signaling pool, there will be no more tasks, waiting for current tasks to finish
+    vec_pointer.clear(); // cleaning argument memory
+
+
+    std::ofstream ofile("output");
+    for (int i = 0; i < n; i++) {
+        ofile << arr[i] << " ";
+    }
+    ofile.close();
+
+    std::cout << std::endl;
     return 0;
 }
---
+++ threadpool.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "thread_safe_queue.hpp"
+#include "thread_joiner.hpp"
+
+#include <zconf.h>
+#include <vector>
+#include <unistd.h>
+#include <functional>
+#include <atomic>
+#include <thread>
+#include <condition_variable>
+#include <iostream>
+#include <sstream>
+
+typedef void (*function_pointer)(void *);
+
+class ThreadPool {
+public:
+    ThreadPool() : done(false), soft_finish(false), joiner(threads) {
+        unsigned const thread_count = std::thread::hardware_concurrency();
+        num_undone = thread_count;
+        try {
+            for (unsigned i = 0; i < thread_count; ++i) {
+                threads.emplace_back(std::thread(&ThreadPool::worker_thread, this));
+            }
+        }
+        catch (...) {
+            done = true;
+            throw;
+        }
+    }
+
+    void submit(function_pointer f, void *args) {
+        work_queue.push(std::make_pair(f, args));
+    }
+
+    void abort_work() { // exit as soon as possible
+        done = true;
+        std::unique_lock<std::mutex> lck(mut);
+        finished.wait(lck);
+    }
+
+    void finish_work() { // exit as all work finishes
+        soft_finish = true;
+        std::unique_lock<std::mutex> lck(mut);
+        finished.wait(lck);
+    }
+
+private:
+    std::atomic_bool done; // flag not to wait or take any more tasks
+    std::atomic_bool soft_finish; // flag that there will be no more new threads
+    std::atomic_int num_undone; // keeping track of threads still in work
+
+
+    ThreadSafeQueue<std::pair<function_pointer, void *>> work_queue;
+    std::vector<std::thread> threads;
+    std::condition_variable finished;
+    std::mutex mut;
+
+    void worker_thread() {
+        while (!done) {
+            std::pair<function_pointer, void *> task;
+            int a = 0;
+            if (work_queue.try_pop(task)) {
+                task.first(task.second);
+            } else {
+                if (soft_finish) {
+                    done = true;
+                } else {
+                    std::this_thread::yield();
+                }
+            }
+        }
+        num_undone--;
+        if (done && !num_undone) {
+            finished.notify_all();
+        }
+    }
+
+    join_threads joiner;
+};
