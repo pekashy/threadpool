937fef4a3ab33bafe4450cf93b96054288b2c9ff
Egor Zyazev <zyazev.ea@phystech.edu>
2020-01-20 21:04:21 +0300

    Bitonic sort now works with any size of array

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
-
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -38,13 +38,13 @@ struct arg {
     }
 
     T *array;
-    int low;
-    int cnt;
-    int dir;
+    int low;  // low border for merge
+    int cnt;  // cnt = k *2, k is number of iterations
+    int dir;  // direction
     std::mutex *m;
-    std::condition_variable *cond;
-    bool *cond_ready; // not to miss the notify
-    bool blocking;
+    std::condition_variable *cond;  // to sync before merge
+    bool *cond_ready;  // not to miss the notify
+    bool blocking;  // on each stage of we run one part in current part and one in queue
     size_t len;
 };
 
@@ -67,9 +67,9 @@ void bitonicMerge(int a[], int low, int cnt, int dir) {
 }
 
 void bitonicSort(void *args) {
-    std::stringstream ss;
+    /*std::stringstream ss;
     ss << std::this_thread::get_id();
-    uint64_t id = std::stoull(ss.str());
+    uint64_t id = std::stoull(ss.str());*/
 
     int cnt = ((arg<int> *) args)->cnt;
     int *a = ((arg<int> *) args)->array;
@@ -90,41 +90,39 @@ void bitonicSort(void *args) {
     auto args2 = new arg<int>(a, low + k, k, 0, m1_new, c1, true, &c_ready);
 
     if (cnt > 1) {
-        tp.submit(&bitonicSort, args1);
-        bitonicSort(args2);
+        if (tp.submit(&bitonicSort, args1)) { // part 1
+            // printf("%d Sorted %lu %d  %d %d\n", blocking, id, cnt, *cond_ready, tp.getNumBusy());
+            std::sort(a + low, a + low + cnt); // sort to the end in case we out of threads
+        } else {
+            // printf("%d Not Sorted %lu %d  %d\n", blocking, id, cnt, *cond_ready);
+            bitonicSort(args2);  // part 2
+        }
         bitonicMerge(a, low, cnt, dir);
-
-        if (blocking) {
-            std::unique_lock<std::mutex> lck(*m);
-            //printf("%d Waiting %lu %d\n", blocking, id, cnt);
-            cnd->wait(lck, [cond_ready]() { return *cond_ready; });
-            fflush(stdout);
-            //printf("%d Passed %lu %d\n", blocking, id, cnt);
-            fflush(stdout);
+        if (blocking) {  // this part called for part 2
+            std::unique_lock<std::mutex> lck(*m); // locks mutex
+            // printf("%d Waiting 2 %lu %d  %d %d\n", blocking, id, cnt, *cond_ready, tp.getNumBusy());
+            // fflush(stdout);
+            // wait unlocks lock and waits for separate thread to notify variable
+            cnd->wait(lck, [cond_ready]() { return *cond_ready; });  // cond_ready helps not to miss the notification
+            // printf("%d Passed %lu %d\n", blocking, id, cnt);
+            // fflush(stdout);
         }
     }
-    if (!blocking) {
+    if (!blocking) {  // this - is for part 1
+        // printf("%d Waiting 1 %lu %d\n", blocking, id, cnt);
+        //fflush(stdout);
         std::unique_lock<std::mutex> lck(*m);
-        *cond_ready = true;
-        cnd->notify_one();
-        fflush(stdout);
-        //printf("%d Notified %lu %d\n", blocking, id, cnt);
-        fflush(stdout);
-    }
-    else {
-        /*free(args1);
+        *cond_ready = true;  // in case part 2 not got to wait block yet
+        cnd->notify_one();  // allow part 2 to pass
+        // printf("%d Notified %lu %d\n", blocking, id, cnt);
+        //fflush(stdout);
+    } else {
+        free(args1);
         free(args2);
         free(m1_new);
-        free(c1);*/
+        free(c1);
     }
-    //printf("%d Finished %lu %d\n", blocking, id, cnt);
-    /*free(args1);
-    free(args2);
-    free(m1_new);
-    free(c1);*/
 
-    //cnd->notify_one();
-    //sleep(1);
 }
 
 int main() {
@@ -138,16 +136,12 @@ int main() {
     }
     ifile.close();
 
-    //std::vector<a::argint> *> vec_pointer; // keeping track of memory we allocated
     std::mutex m;
     std::condition_variable cnd;
     bool cond_ready = false;
     auto args = new arg<int>(arr, 0, n, 1, &m, &cnd, false, &cond_ready);
     bitonicSort(args);
-    //merge_sort(tp, vec_pointer, arr, buff, 0, n);
-    //printf("waiting for pool\n");
-    tp.finish_work(); // signaling pool, there will be no more tasks, waiting for current tasks to finish
-    //vec_pointer.clear(); // cleaning argument memory
+    tp.finishWork(); // signaling pool, there will be no more tasks, waiting for current tasks to finish
 
 
     std::ofstream ofile("output");
@@ -155,9 +149,7 @@ int main() {
         ofile << arr[i] << " ";
         cout << arr[i] << " ";
     }
-
     ofile.close();
-
     std::cout << std::endl;
     return 0;
 }
--- threadpool.hpp
+++ threadpool.hpp
@@ -18,11 +18,12 @@ typedef void (*function_pointer)(void *);
 class ThreadPool {
 public:
     ThreadPool() : done(false), soft_finish(false), joiner(threads) {
-        unsigned const thread_count = 64;
-        num_undone = thread_count;
+        thread_count = std::thread::hardware_concurrency();
+        num_busy = 0;
+
         try {
             for (unsigned i = 0; i < thread_count; ++i) {
-                threads.emplace_back(std::thread(&ThreadPool::worker_thread, this));
+                threads.emplace_back(std::thread(&ThreadPool::workerThread, this));
             }
         }
         catch (...) {
@@ -31,17 +32,28 @@ public:
         }
     }
 
-    void submit(function_pointer f, void *args) {
-        work_queue.push(std::make_pair(f, args));
+    int submit(function_pointer f, void *args) {
+        std::unique_lock<std::mutex> lck(subm_mut);
+
+        if (num_busy < thread_count) { // here we need a mutex
+            num_busy++;
+            work_queue.push(std::make_pair(f, args));
+            return 0;
+        }
+        return 1;
     }
 
-    void abort_work() { // exit as soon as possible
+    int getNumBusy() {
+        return num_busy;
+    }
+
+    void abortWork() { // exit as soon as possible
         done = true;
         std::unique_lock<std::mutex> lck(mut);
         finished.wait(lck);
     }
 
-    void finish_work() { // exit as all work finishes
+    void finishWork() { // exit as all work finishes
         soft_finish = true;
         std::unique_lock<std::mutex> lck(mut);
         finished.wait(lck, [this]() { return this->ready; });
@@ -50,20 +62,24 @@ public:
 private:
     std::atomic_bool done; // flag not to wait or take any more tasks
     std::atomic_bool soft_finish; // flag that there will be no more new threads
-    std::atomic_int num_undone; // keeping track of threads still in work
+    std::atomic_int num_busy; // keeping track of threads still in work
     bool ready = false;
 
     ThreadSafeQueue<std::pair<function_pointer, void *>> work_queue;
     std::vector<std::thread> threads;
     std::condition_variable finished;
     std::mutex mut;
+    unsigned thread_count;
 
-    void worker_thread() {
+    std::mutex subm_mut;
+
+    void workerThread() {
         while (!done) {
             std::pair<function_pointer, void *> task;
             int a = 0;
             if (work_queue.try_pop(task)) {
                 task.first(task.second);
+                num_busy--;
             } else {
                 if (soft_finish) {
                     done = true;
@@ -72,8 +88,7 @@ private:
                 }
             }
         }
-        num_undone--;
-        if (done && !num_undone) {
+        if (done && !num_busy) {
             std::unique_lock<std::mutex> lck(mut);
             ready = true;
             finished.notify_all();
